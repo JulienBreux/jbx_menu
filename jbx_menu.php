2d3b6db4b2bb13a15b28782ebe9b18b829d626e2
Julien BREUX <julien.breux@gmail.com>
2011-08-11 09:52:49 +0200

    Change: Version

--- jbx_menu.php
+++ jbx_menu.php
@@ -43,7 +43,7 @@ class jbx_menu extends Module
 	{
 		$this->name = 'jbx_menu';
 		$this->tab = 'front_office_features';
-		$this->version = '2.8.0';
+		$this->version = '2.8.1';
 		$this->author = 'Web-Scope';
 
 		parent::__construct();
